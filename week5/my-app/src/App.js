a75cdd2cd70da93ad227c9461ef5fa2d0bef3785
fatima22234 <emaaaa37@gmail.com>
2025-09-21 22:56:03 +0500

    week 5

---
+++ week5/my-app/src/App.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import Header from "./components/Header";
+import Tasks from './components/Tasks';
+import { FaAsterisk } from 'react-icons/fa';
+
+
+function App() {
+  const [tasks, setTasks] = useState(
+[
+    {
+      "id": 1,
+      "text": "Doctors Appointment",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": true
+    },
+    {
+      "id": 2,
+      "text": "Meeting at School",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    }
+  ]
+  )
+
+  const deleteTask = (id)=>{
+    setTasks(tasks.filter((task)=>task.id!==id))
+    
+  }
+  const toggleReminder=(id)=>{
+    setTasks(tasks.map((task)=>(task.id==id? {...task, reminder:!task.reminder}:task)))
+
+  } 
+
+  return (
+    <div className="App container">
+     <Header title="Task Tracker"/>
+     {tasks.length>0? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> :"no tasks to be shown"}
+    </div>
+  );
+}
+
+export default App;
